14a36f6a98bb1df4677b076421f7d091ce386e45
kkkatherine323 <xiaotz7@uw.edu>
2022-11-14 23:49:15 -0800

    summary_information.R

--- source/summary_information.R
+++ source/summary_information.R
@@ -10,15 +10,16 @@ library("tidyr")
 data_summary <- read.csv("https://raw.githubusercontent.com/kkkatherine323/project-itsyenmyvo22/main/data/all_sum_df.csv")
 
 summary_info <- list()
-summary_info$deaths_sales_18 <- sum(data_summary$age18longgunsale, na.rm = T)
-summary_info$deaths_sales_21 <- sum(data_summary$age21longgunsale, na.rm = T)
+summary_info$deaths_sales_18 <- sum(data_summary$age18longgunsale, na.rm = T) 
+summary_info$deaths_sales_21 <- sum(data_summary$age21longgunsale, na.rm = T) 
 summary_info$max_deaths_state <- data_summary %>%
   filter(n_killed == max(n_killed, na.rm = T)) %>%
-  select(state)
+  pull(state) 
 summary_info$highest_rate_state <- data_summary %>%
   filter(Percent.Above.Poverty.Rate == max(Percent.Above.Poverty.Rate, na.rm = T)) %>%
-  select(state) %>%
-  distinct(state, .keep_all = TRUE)
+  pull(state) %>%
+  unique(summary_info, highest_rate_state = FALSE)
 summary_info$highest_deaths_one_day <- data_summary %>%
   filter(n_killed == max(n_killed, na.rm = T)) %>%
-  select(n_killed)
+  pull(n_killed)
+?unique
