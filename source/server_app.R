ec398ed22523268023a6406b639ab03afaec7440
kkkatherine323 <xiaotz7@uw.edu>
2022-12-07 21:24:10 -0800

    server_app.R

---
+++ source/server_app.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(hrbrthemes)
+library(viridisLite)
+#install.packages("hrbrthemes")
+library("dplyr")
+library(lubridate)
+
+all_sum_df<-read.csv(url("https://raw.githubusercontent.com/info201b-au2022/project-itsyenmyvo22/main/data/all_sum_df.csv"))
+nat_data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-itsyenmyvo22/main/data/nat_data.csv")%>%
+  mutate(state = tolower(state))
+#View(nat_data)
+
+guns <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-itsyenmyvo22/main/data/all_sum_df.csv", stringsAsFactors = F)%>%
+  filter(date > '01-01-2017')%>%
+  select(state, n_killed)
+
+server <- function(input, output) {
+  
+  #Since data has so many variables, I simplified it to data on a monthly basis.
+  data_killed_injured<-all_sum_df %>%
+    select(date, n_killed, n_injured)%>%
+    mutate(year = year(date), month = month(date))%>%
+    group_by(year, month) %>%
+    summarise(sum_n_injured = sum(n_injured),sum_n_killed = sum(n_killed))
+  data_killed_injured<-unite(data_killed_injured, col=date, c("year","month"), sep="/",remove=TRUE)
+  
+  
+  
+  colors <- c("n_killed" = "blue", "n_injured" = "red")
+  
+  Killed_injured<- ggplot(data_killed_injured, aes(x = date)) +
+    geom_line(aes(y = sum_n_killed, color = "n_killed", group=1)) +
+    geom_line(aes(y = sum_n_injured, color = "n_injured",group=1)) +
+    labs(x = "date",
+         y = "ppl",
+         color = "type") +
+    scale_color_manual(values = colors)+
+    scale_x_discrete(breaks = c("2013/1","2014/1","2015/1","2016/1","2017/1","2018/1"))
+  
+  ## Define server logic
+  server <- function(input, output) {
+    output$line <- renderPlot({
+      p <- ggplot(data_killed_injured, aes(x = date)) +
+        theme_bw()+
+        scale_x_discrete(breaks = c("2013/1","2014/1","2015/1","2016/1","2017/1","2018/1"))
+      if (input$slider[1] == 1) {
+        p <- p + geom_line(aes(y = sum_n_killed, color = "n_killed", group=1)) 
+      }
+      if (input$slider[2] == 2) {
+        p <- p + geom_line(aes(y = sum_n_injured, color = "n_injured", group=1))
+      }
+      p <- p + labs(x = "date",
+                    y = "ppl",
+                    color = "type") +
+        scale_color_manual(values = colors)
+      print(p)
+    })
+    
+  }
+}
