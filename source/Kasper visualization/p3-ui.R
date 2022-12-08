36ce14b3af969a8494b044a2ae6bd1cea61d1f83
2470572372 <2470572372@qq.com>
2022-12-07 18:53:42 -0800

    Kasper

--- source/Kasper visualization/p3-ui.R
+++ source/Kasper visualization/p3-ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 ## Define UI for application
-ui <- fluidPage(
+line_panel<-tabPanel(
+  "line plot",
   
   # Application title
   titlePanel("The number of gun violence killed and injured"),
@@ -17,4 +18,10 @@ ui <- fluidPage(
       p("This line graph shows the number of deaths and injuries caused by gun violence in the United States from January 2013 to March 2018.")
     )
   )
+)
+
+ui <- navbarPage(
+  "line plot ",
+  
+  line_panel
 )
